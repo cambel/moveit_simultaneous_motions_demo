49937d75b663f384bac1f3155d64279bf1f1c5ce
Cristian Beltran <cristianbehe@gmail.com>
2022-07-06 14:24:53 +0900

    Set simultaneous trajectories with AsyncExecute()

--- panda_dual_arm_demo/src/simple_path_simultaneous.cpp
+++ panda_dual_arm_demo/src/simple_path_simultaneous.cpp
@@ -19,21 +19,23 @@ int main(int argc, char** argv) {
   moveit::planning_interface::MoveGroupInterface dual_arm_group("dual_arm");
   moveit::planning_interface::MoveGroupInterface panda_1_group("panda_1");
   moveit::planning_interface::MoveGroupInterface panda_2_group("panda_2");
-  
+
   dual_arm_group.setPlanningTime(0.5);
 
-  ROS_INFO_STREAM("Panda_1 initial pose" << panda_1_group.getCurrentPose("panda_1_link8") << "\n");
-  ROS_INFO_STREAM("Panda_2 initial pose" << panda_2_group.getCurrentPose("panda_2_link8") << "\n");
+  ROS_INFO_STREAM("Panda_1 initial pose"
+                  << panda_1_group.getCurrentPose("panda_1_link8") << "\n");
+  ROS_INFO_STREAM("Panda_2 initial pose"
+                  << panda_2_group.getCurrentPose("panda_2_link8") << "\n");
 
   geometry_msgs::PoseStamped panda_1_pose_goal;
   geometry_msgs::PoseStamped panda_2_pose_goal;
   geometry_msgs::Pose panda_1_pose;
   geometry_msgs::Pose panda_2_pose;
-  panda_1_pose_goal.header.frame_id = "base";
-  panda_2_pose_goal.header.frame_id = "base";
+  panda_1_pose_goal.header.frame_id = "panda_1_link1";
+  panda_2_pose_goal.header.frame_id = "panda_2_link1";
 
   panda_1_pose.position.x = 0.450;
-  panda_1_pose.position.y =-0.499924;
+  panda_1_pose.position.y = -0.50;
   panda_1_pose.position.z = 1.600;
   panda_1_pose.orientation.x = 0.993436;
   panda_1_pose.orientation.y = 3.5161e-05;
@@ -41,29 +43,42 @@ int main(int argc, char** argv) {
   panda_1_pose.orientation.w = 2.77577e-05;
 
   panda_2_pose.position.x = 0.450;
-  panda_2_pose.position.y = 0.499964;
+  panda_2_pose.position.y = 0.40;
   panda_2_pose.position.z = 1.600;
   panda_2_pose.orientation.x = 0.993434;
   panda_2_pose.orientation.y = -7.54803e-06;
   panda_2_pose.orientation.z = 0.114403;
   panda_2_pose.orientation.w = 3.67256e-05;
 
-  dual_arm_group.clearPoseTargets();
-  dual_arm_group.setStartStateToCurrentState();
+  moveit::planning_interface::MoveGroupInterface::Plan my_plan_1;
+  moveit::planning_interface::MoveGroupInterface::Plan my_plan_2;
+
+  // Plan with first joint group
+  panda_1_group.clearPoseTargets();
+  panda_1_group.setStartStateToCurrentState();
   std::string panda_1_eff = "panda_1_link8";
-  dual_arm_group.setPoseTarget(panda_1_pose, panda_1_eff);
+  panda_1_group.setPoseTarget(panda_1_pose, panda_1_eff);
 
+  // Plan with second joint group
+  panda_2_group.clearPoseTargets();
+  panda_2_group.setStartStateToCurrentState();
   std::string panda_2_eff = "panda_2_link8";
-  dual_arm_group.setPoseTarget(panda_2_pose, panda_2_eff);
+  panda_2_group.setPoseTarget(panda_2_pose, panda_2_eff);
 
-  moveit::planning_interface::MoveGroupInterface::Plan my_plan;
-  auto res = dual_arm_group.plan(my_plan);
+  auto res = panda_1_group.plan(my_plan_1);
   bool success = (res == moveit::planning_interface::MoveItErrorCode::SUCCESS);
   if (!success) {
-    ROS_INFO("Plan did not succeeded");
-  } else {
-    dual_arm_group.execute(my_plan);
+    ROS_INFO("Plan1 did not succeeded");
   }
+
+  res = panda_2_group.plan(my_plan_2);
+  success = (res == moveit::planning_interface::MoveItErrorCode::SUCCESS);
+  if (!success) {
+    ROS_INFO("Plan2 did not succeeded");
+  }
+
+  panda_1_group.asyncExecute(my_plan_1);
+  panda_2_group.asyncExecute(my_plan_2);
 
   ros::shutdown();
 }
